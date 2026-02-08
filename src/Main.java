372d0109ab48e1bb1885d6ff50e5ba4075021ec9
rohansoni100304-lab <rohansoni100304@gmail.com>
2026-02-08 23:55:31 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args){
+        PersonalDetail obj = new PersonalDetail("riya");
+        PersonalDetail obj1 = new PersonalDetail("rohan");
+        obj.getName();
+        obj1.getName();
+        // obj1.setName("Rohit");
+        obj1.getName();
+    }
+}
---
+++ src/PersonalDetail.java
@@ -0,0 +1,18 @@
+public class PersonalDetail {
+
+    String name;
+
+    PersonalDetail(String n){
+        name = n;
+    }
+
+    void getName(){
+        System.out.println("My name is " + name); }
+    void setName(String n){
+        name = n;
+    }
+
+}
+
+
+
